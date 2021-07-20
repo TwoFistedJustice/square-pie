99a82b5746a4c8287ff37e37a4d1c3f0c56bafd1
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-07-19 18:57:21 -0700

    Begin implementing idempotency - having problems

--- src/square.jsw.js
+++ src/square.jsw.js
@@ -1,5 +1,6 @@
 import { fetch } from 'wix-fetch';
 import { getSecret } from 'wix-secrets-backend'
+const { v4: uuidv4 } = require('uuid');
 
 const config = {
   squareVersion: '2021-06-16',
@@ -24,7 +25,7 @@ var bodyTestcase = {
 class SquareRequest {
   
   constructor(isProduction) {
-    this.isProduction = isProduction
+    this.isProduction = isProduction;
   }
   
   // COMPUTED PROPERTIES
@@ -45,8 +46,6 @@ class SquareRequest {
   }
   
   // METHODS
-  // generate idempotency_key
-  
   
   headers(secret) {
     return {
@@ -73,24 +72,21 @@ class SquareRequest {
   }
 } // END class
 
-
 // LEVEL TWO CLASSES
 
-class List extends SquareRequest{
-constructor(isProduction) {
-  super(isProduction);
-  this.endpoint = ''
-}
-options(secret) {
-  return {
-    method: 'get',
-    headers: this.headers(secret),
-    body: body
+class List extends SquareRequest {
+  constructor(isProduction) {
+    super(isProduction);
+    this.endpoint = ''
   }
-}
+  options(secret) {
+    return {
+      method: 'get',
+      headers: this.headers(secret),
+      body: body
+    }
+  }
 } // END class
-
-
 
 // to extend this
 // override the options method in the subclass
@@ -110,28 +106,46 @@ class RetrieveUpdateDelete extends SquareRequest {
     return options;
   }
   // METHODS
-  set id (someId){
+  set id(someId) {
     this.endpoint = `/${someId}`;
   }
 } // END class
 
 
+
+
+
+
+
+// TODO
+//getting: Cannot set property body of #<SquareRequest> which has only a getter
+
+
 // creates a whole new document
 // you tell it what to store
-class Create extends SquareRequest{
+class Create extends SquareRequest {
   constructor(isProduction) {
     super(isProduction);
     this.endpoint = ''
+    this.idempotency_key = uuidv4();
     this.body = {}
   }
   options(secret) {
+    console.log(this.idempotency_key);
     return {
       method: 'post',
       headers: this.headers(secret),
       body: this.body
     }
   }
+  
+  get getIdempotency_key() {
+    return this.idempotency_key;
+  }
+  
 } // END class
+
+
 
 
 
@@ -158,11 +172,11 @@ class CustomerRetrieve extends RetrieveUpdateDelete {
 }
 
 class CustomerDelete extends RetrieveUpdateDelete {
-  constructor (isProduction) {
-    super (isProduction);
+  constructor(isProduction) {
+    super(isProduction);
     this.apiName = 'customers';
   }
-  options(secret){
+  options(secret) {
     let options = super.options(secret);
     options.method = 'delete';
     return options;
@@ -170,20 +184,32 @@ class CustomerDelete extends RetrieveUpdateDelete {
 }
 
 
+
+
 class CustomerCreate extends Create {
-  constructor (isProduction) {
-    super (isProduction);
+  constructor(isProduction) {
+    super(isProduction);
     this.apiName = 'customers';
-    this.body = {
-      idempotency_key: '',
-      given_name: '',
-      family_name: '',
-    }
   }
+  
+  
+  //METHODS
+  populate(customer) {
+    console.log(this.getIdempotency_key);
+    this.body = customer;
+    body.idempotency_key = this.idempotency_key;
+  }
+  
 }
 
 
 
+
+
+
+
+
+// create a customer class which includes email validation
 
 // the function that calls the async getter MUST be async
 // that way they go into the same call stack
@@ -197,9 +223,7 @@ export async function testList() {
   return true;
 }
 
-
-
-export async function testRetrieve(){
+export async function testRetrieve() {
   let testCustomerSqID = "CJ5Z66RDA4ZR3AQVCY1SRW4ZW8";
   let retrieve = new CustomerRetrieve(false);
   let secret = await getSecret(retrieve.secretName);
@@ -209,18 +233,51 @@ export async function testRetrieve(){
   return customer.customer;
 }
 
+export async function testCreate() {
+  let someGuy = new CustomerCreate(false);
+  let secret = await getSecret(someGuy.secretName);
+  someGuy.populate = {
+    given_name: "Phillipe",
+    family_name: "Dacreep",
+    company_name: "Auntie Susan's Mobile Ice Cream",
+    nickname: "wanted in 5 systems",
+    email_address: "lilpp@iscream.org",
+    address: {
+      address_line1: "15 Elm Street",
+      address_line2: undefined,
+      address_line3: undefined,
+      administrative_district_level_1: "NY",
+      administrative_district_level_2: undefined,
+      administrative_district_level_3: undefined,
+      country: "US",
+      first_name: "Phil",
+      last_name: "D",
+      locality: "Amityville",
+      oranization: "Auntie Sue's",
+      postal_code: "00100",
+      sublocality: undefined,
+      sublocality_2: undefined,
+      sublocality_3: undefined
+    },
+    phone_number: "212-ruh-roh!",
+    reference_id: "some identifier",
+    note: "walk a mile in his shoes, go to jail",
+    birthday: "1998-09-21T00:00:00-00:00"
+  };
+  
+  let response = await someGuy.makeRequest(secret);
+  console.log(response);
+  return response;
+}
 
-export async function testDelete(){
+// set this up to create a customer, log that customer, then delete it
+export async function testDelete() {
   let testCustomerSqID = "";
+  
   let vaporizeMe = new CustomerDelete(false);
   let secret = await getSecret(vaporizeMe.secretName);
   vaporizeMe.id = testCustomerSqID;
   let vaporized = await vaporizeMe.makeRequest(secret);
   console.log(vaporized);
   
-  
-  
 }
-
-
-
