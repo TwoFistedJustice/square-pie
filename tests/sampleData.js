27a6be0ca381dfbf068253bdcfa7a5d32bff3ce8
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-08-08 17:25:41 -0700

    chore: tweak sample customers

--- tests/sampleData.js
+++ tests/sampleData.js
@@ -1,69 +1,10 @@
-const mikey = {
-  given_name: "Michael",
-  family_name: "Myers",
-  email_address: "candyTime@iscream.org",
-  address: {
-    address_line_1: "86 Nowhere Is Safe Court",
-    address_line_2: "",
-    locality: "Haddonfield",
-    administrative_district_level_1: "IL",
-    postal_code: "60686",
-    country: "US",
-  },
-  phone_number: "1-937-555-1031",
-  reference_id: "Halloween-31",
-  note: "want some candy?",
-  preferences: {
-    email_unsubscribed: false,
-  },
-};
-
-const buffy = {
-  given_name: "Buffy",
-  family_name: "Summers",
-  email_address: "buffy@magicbox.com",
-  address: {
-    address_line_1: "1630 Revello Drive",
-    locality: "Sunnydale",
-    administrative_district_level_1: "CA",
-    postal_code: "95037",
-    country: "US",
-  },
-  phone_number: "805-555-0833",
-  reference_id: "1997-2003",
-  note: "When did I get a sister?",
-  preferences: {
-    email_unsubscribed: false,
-  },
-};
-
-const jason = {
-  given_name: "Jason",
-  family_name: "Voorhees",
-  email_address: "machete@iscream.org",
-  address: {
-    address_line_1: "1054 Camp Crystal Lake Street",
-    address_line_2: "Cabin 86",
-    locality: "Crystal Lake",
-    administrative_district_level_1: "OH",
-    postal_code: "43086",
-    country: "US",
-  },
-  phone_number: "1-937-555-0013",
-  reference_id: "Friday-The-13th",
-  note: "naughty children go to sleep",
-  preferences: {
-    email_unsubscribed: false,
-  },
-};
-
 const sampleCustomers = function () {
   return {
-    shorthand: {
+    amelia: {
       given_name: "Amelia",
       email_address: "aMe.lia698214271522544412252474@gmail.com",
     },
-    spiritual: {
+    freddie: {
       given_name: "Freddie",
       family_name: "Krueger",
       email_address: "freddie@iscream.org",
@@ -82,13 +23,12 @@ const sampleCustomers = function () {
         email_unsubscribed: false,
       },
     },
-    stoneage: {
+    fred: {
       given_name: "Fred",
       family_name: "Flintsone",
       email_address: "fred@slaterock.com",
       address: {
         address_line_1: "301 Cobblestone Way",
-        address_line_2: "",
         locality: "Bedrock",
         administrative_district_level_1: "BR",
         postal_code: "70777",
@@ -101,7 +41,7 @@ const sampleCustomers = function () {
         email_unsubscribed: false,
       },
     },
-    unhappy: {
+    jason: {
       given_name: "Jason",
       family_name: "Voorhees",
       email_address: "machete@iscream.org",
@@ -126,7 +66,6 @@ const sampleCustomers = function () {
       email_address: "candyTime@iscream.org",
       address: {
         address_line_1: "86 Nowhere Is Safe Court",
-        address_line_2: "",
         locality: "Haddonfield",
         administrative_district_level_1: "IL",
         postal_code: "60686",
@@ -145,7 +84,6 @@ const sampleCustomers = function () {
       email_address: "buffy@magicbox.com",
       address: {
         address_line_1: "1630 Revello Drive",
-        address_line_2: "",
         locality: "Sunnydale",
         administrative_district_level_1: "CA",
         postal_code: "95037",
@@ -162,8 +100,5 @@ const sampleCustomers = function () {
 };
 
 module.exports = {
-  buffy,
-  mikey,
-  jason,
   sampleCustomers,
 };
