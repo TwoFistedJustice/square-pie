474f53a7e4730f6a8b00fcfa0cb1953e01fda1fa
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-08-14 23:01:08 -0700

    tests: add test for Category

--- tests/catalog_object.test.js
+++ tests/catalog_object.test.js
@@ -1,18 +1,37 @@
 "use strict";
 const should = require("chai").should();
-const { Category } = require("../src/catalog_object_classes");
+const { Helper_Name, Category } = require("../src/catalog_object_classes");
 
 describe("Empty block", () => {
   test("Should pass because it is an empty test", () => {});
 });
 
-describe("Catalog Category", () => {
-  const schleeb = new Category("schleeb");
+describe("Catalog: Helper_Name", () => {
+  const helper = new Helper_Name();
+  let just_right = "Just right";
   let too_long =
     "ouwoiuwolakrusdspfvjknktuceglhhyemavhppauktqgxwnhfsfuvgdtrjxxcsuorblgukzyibvqdetdwetfjzvyooejafifnfnsqiqguojlthnrjoxxcimbqdkzrvmbtixiflmvkxcvtagwkyyelpahhmzciabaxcnjenaublzpzddkotoxcddsydyceggamrsuukxacgrqymmtogglpufrvfhoiueijldihluswluymcphzhmyzbpjwpjwcqnaawfhnn";
-  test.only("Should reject a value that is more than 255 characters", () => {
+  test("Should accept a value that is less than 255 characters", () => {
     expect(() => {
-      schleeb.name = too_long;
+      helper.name = just_right;
+    }).not.toThrow();
+  });
+  test("Should reject a value that is more than 255 characters", () => {
+    expect(() => {
+      helper.name = too_long;
     }).toThrow();
+  });
+});
+
+describe("Catalog: Category", () => {
+  const name = "Thing";
+  const testSubject = new Category(name);
+  // const testValue = testSubject.parcel();
+  const expected = {
+    type: "CATEGORY",
+    name: name,
+  };
+  test.only("Should have the expected name and type.", () => {
+    expect(testSubject.parcel()).toMatchObject(expected);
   });
 });
