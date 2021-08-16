1aa008fd76f795e983e3582695cb1961c0687990
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-08-15 19:56:43 -0700

    test: implement tests for Catalog_Object_Wrapper

--- tests/catalog_object.test.js
+++ tests/catalog_object.test.js
@@ -1,9 +1,81 @@
 "use strict";
 const should = require("chai").should();
-const { Helper_Name, Category } = require("../src/catalog_object_classes");
+const { v4: uuidv4 } = require("uuid");
+uuidv4();
+
+const {
+  Catalog_Object_Wrapper,
+  Helper_Name,
+  Category,
+} = require("../src/catalog_object_classes");
 
 describe("Empty block", () => {
   test("Should pass because it is an empty test", () => {});
+});
+
+describe("Catalog Object Wrapper", () => {
+  const arr = [{ a: 0 }, { b: 1 }, { c: 2 }, { d: 3 }, { e: 4 }];
+
+  test("Payload should be an object when given a single object", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing.attach(arr[0]);
+    expect(thing.payload).toMatchObject(arr[0]);
+  });
+
+  test("Payload should be an array of objects when given a more than one object", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing
+      .attach(arr[0])
+      .attach(arr[1])
+      .attach(arr[2])
+      .attach(arr[3])
+      .attach(arr[4]);
+    expect(thing.payload).toMatchObject(arr);
+  });
+
+  test("Add alias should function exactly as attach method", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing.add(arr[0]).add(arr[1]).add(arr[2]).add(arr[3]).add(arr[4]);
+    expect(thing.payload).toMatchObject(arr);
+  });
+
+  test("Attach method and add alias should work together", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing.add(arr[0]).attach(arr[1]).add(arr[2]).attach(arr[3]).add(arr[4]);
+    expect(thing.payload).toMatchObject(arr);
+  });
+
+  test("Idempotency key should be a proper UUID", () => {
+    const { validate } = require("uuid");
+    let thing = new Catalog_Object_Wrapper();
+    thing.add(arr[0]);
+    thing.finalize();
+    validate(thing.fardel.idempotency_key).should.be.true;
+  });
+
+  test("Fardel should be in correct form with one object", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing.add(arr[0]);
+    thing.finalize();
+    // test vs object
+    Object.prototype.hasOwnProperty.call(thing.fardel, "object").should.be.true;
+    thing.fardel.object.should.be.an("object");
+  });
+
+  test("Fardel should be in correct form with multiple object", () => {
+    let thing = new Catalog_Object_Wrapper();
+    thing.add(arr[0]);
+    thing.attach(arr[1]);
+    thing.finalize();
+    //test vs array
+    Object.prototype.hasOwnProperty.call(thing.fardel, "objects").should.be
+      .true;
+    thing.fardel.objects.should.be.an("array");
+  });
+
+  // test("", () => {
+  //   let thing = new Catalog_Object_Wrapper();
+  // });
 });
 
 describe("Catalog: Helper_Name", () => {
@@ -40,7 +112,7 @@ describe("Catalog: Category", () => {
       name: name,
     },
   };
-  test.only("Should have the expected name and type.", () => {
+  test("Should have the expected name and type.", () => {
     expect(testSubject.parcel()).toMatchObject(expected);
   });
 });
