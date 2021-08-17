798b5761b7463ae864a7d5222d56fb7feec95005
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-08-17 13:37:41 -0700

    chore: rough out catalog item class

--- src/lib/catalog_object_item.js
+++ src/lib/catalog_object_item.js
@@ -38,20 +38,6 @@ or make a method called 'variation' and curry it
 //     this._team_member_ids;
 //     this._stockable_coversion;
 //   }
-//   // GETTERS
-//   get id() {
-//     return this._item_id;
-//   }
-//   get type() {
-//     return this._type;
-//   }
-//   set type(bool) {
-//     this._type = "ITEM_VARIATION";
-//   }
-//
-//   set id(id) {
-//     this._item_id = id;
-//   }
 //   // new standard name for chain setters
 //   spawn() {}
 // }
@@ -67,21 +53,118 @@ class Catalog_Item extends Helper_Name {
     this._available_online;
     this._available_for_pickup;
     this._available_electronically;
-    this._tax_ids;
-    this._modifier_list_info;
-    this._variations;
+    this._tax_ids = [];
+    this._modifier_list_info = [];
+    this._variations = [];
     this._product_type = "REGULAR"; // make a switcher
     this._skip_modifier_screen; //default is false
-    this._item_options;
+    this._item_options = [];
     this._sort_name = undefined; // supported in Japan only
   }
 
+  // GETTERS
   get type() {
     return this._type;
   }
+  get description() {
+    return this._description;
+  }
+  get abbreviation() {
+    return this._abbreviation;
+  }
+  get category_id() {
+    return this._category_id;
+  }
+  get label_color() {
+    return this._label_color;
+  }
+  get available_online() {
+    return this._available_online;
+  }
+  get available_for_pickup() {
+    return this._available_for_pickup;
+  }
+  get available_electronically() {
+    return this._available_electronically;
+  }
+  get tax_ids() {
+    return this._tax_ids;
+  }
+  get modifier_list_info() {
+    return this._modifier_list_info;
+  }
+  get variations() {
+    return this._variations;
+  }
+  get product_type() {
+    return this._product_type;
+  }
+  get skip_modifier_screen() {
+    return this._skip_modifier_screen;
+  }
+  get item_options() {
+    return this._item_options;
+  }
+  get sort_name() {
+    return this._sort_name;
+  }
+
+  // SETTERS
   set type(bool) {
     this._type = "ITEM";
   }
+  set name(val) {
+    // max 512
+  }
+  set description(val) {
+    //max 4096
+  }
+  set abbreviation(val) {
+    //max 24
+  }
+  set label_color(val) {
+    // validate hed color code
+  }
+  set available_online(bool) {
+    this._available_online = bool;
+  }
+  set available_for_pickup(bool) {
+    this._available_for_pickup = bool;
+  }
+  set available_electroncially(bool) {
+    this._available_electroncially = bool;
+  }
+  set category_id(val) {
+    //The ID of the item's category, if any.
+  }
+  set tax_ids(id) {
+    // push the id onto the array
+  }
+  set modifer_list_info(obj) {
+    //validate the object
+    // push the obj onto the array
+  }
+  set variations(obj) {
+    //validate the object
+    // push the obj onto the array
+  }
+  set product_type(val) {
+    // "REGULAR" or "APPOINTMENTS_SERVICE"
+  }
+  set item_options(id) {
+    // max length of 6
+    // push the id obj onto the array
+  }
+  set sort_name(val) {
+    // A name to sort the item by. If this name is unspecified, namely,
+    // the sort_name field is absent, the regular name field is used for sorting.
+  }
+
+  //METHODs
+
+  // for bools have chain propName.yes, propName.no
+
+  // for fixed values make a link for each value
 }
 
 module.exports = Catalog_Item;
