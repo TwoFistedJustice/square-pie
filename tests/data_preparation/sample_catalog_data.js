b95969e2919aae1f897d38db2ce58906836af118
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-08-31 18:55:36 -0700

    test: add sample objects

--- tests/data_preparation/sample_catalog_data.js
+++ tests/data_preparation/sample_catalog_data.js
@@ -1,32 +1,50 @@
 const Catalog_Item = require("../../src/lib/catalog_object_item");
 const Catalog_Item_Variation = require("../../src/lib/catalog_object_item_variation");
+const Catalog_Category = require("../../src/lib/catalog_object_category");
 const Catalog_Item_Wrapper = require("../../src/lib/catalog_object_wrapper");
 
 const wrapper = new Catalog_Item_Wrapper();
-const item = new Catalog_Item();
-const large = new Catalog_Item_Variation();
-const small = new Catalog_Item_Variation();
 
+const category = new Catalog_Category();
+category.name = "Guitars";
+
+const item = new Catalog_Item();
 const itemConfig = item.spawn();
+
+const large = new Catalog_Item_Variation();
 const lg_config = large.spawn();
+
+const small = new Catalog_Item_Variation();
 const sm_config = small.spawn();
 
 itemConfig
-  .name("Fancy Coffee")
+  .id("coffee")
+  .name("FancyCoffeeuo")
   .description("Froufrou Coffee Drink")
   .abbreviation("FC");
 lg_config.name("Large").price_money(675);
+lg_config.name("Large");
 sm_config.name("Small").price_money(550);
 
 itemConfig.variations(large.fardel).variations(small.fardel);
-wrapper.attach(item.fardel);
+itemConfig.variations(large.fardel);
 
-// in case thou needest to see the data
-// this bes a goodly point to stop
-// let outputI = JSON.stringify(item.fardel, null, 2);
+wrapper.attach(item.fardel);
+wrapper.add(category.fardel);
+wrapper.finalize();
+
+// const doit = async function () {
+// const Catalog_Request_Upsert = require("../../src/lib/catalog_request_upsert");
+//   const upsert = new Catalog_Request_Upsert();
+//
+//   upsert.body = wrapper.fardel;
+//   let delivered = await upsert.makeRequest();
+//   console.log(delivered);
+// };
+// doit();
 
 const sample_objects = {
-  coffee: item.fardel,
+  mulitple: wrapper.fardel,
 };
 
 module.exports = sample_objects;
