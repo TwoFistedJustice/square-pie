93546ac850c9010240f9778282979789395fe9a3
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-09-02 16:56:20 -0700

    feat: rough implement catalog list class

---
+++ src/lib/catalog_request_list.js
@@ -0,0 +1,65 @@
+const Catalog_Request = require("./catalog_request");
+const { setter_chain_generator_config } = require("./utilities_curry");
+
+class Catalog_List extends Catalog_Request {
+  constructor() {
+    super();
+    this._method = "get";
+    this._endpoint = "/list";
+
+    this.configuration = {
+      keys: ["types"],
+      types: [
+        "ITEM_VARIATION",
+        "CATEGORY",
+        "DISCOUNT",
+        "TAX",
+        "MODIFIER",
+        "MODIFIER_LIST",
+        "IMAGE",
+      ],
+    };
+    this._fardel = {
+      catalog_version: undefined,
+    };
+    this._delivery;
+  }
+  get fardel() {
+    return this._fardel;
+  }
+  get catalog_version() {
+    return this._fardel.catalog_version;
+  }
+  get delivery() {
+    return this._delivery;
+  }
+
+  // SETTERS
+  set delivery(parcel) {
+    this._delivery = parcel.objects;
+  }
+  // /* catalog version
+  //  * Square uses ISO date format :  str "YYYY-MM-DD"
+  //  * go to their docs to see what their versions are.
+  //  * */
+  set catalog_version(version) {
+    this._fardel.catalog_version = version;
+  }
+
+  // METHODS
+  make() {
+    const methods = () => {
+      const properties = {
+        self: this,
+        catalog_version: function (version) {
+          this.self.catalog_version = version;
+        },
+      };
+      setter_chain_generator_config(this.configuration, properties, this);
+      return properties;
+    };
+    return methods();
+  }
+}
+
+module.exports = Catalog_List;
