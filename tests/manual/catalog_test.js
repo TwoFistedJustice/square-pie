0f0d5355c5f73a863311f1737887d60479bc912d
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-09-03 16:14:55 -0700

    test: add manual test for catalog delete

--- tests/manual/catalog_test.js
+++ tests/manual/catalog_test.js
@@ -1,11 +1,25 @@
 const Catalog_List = require("../../src/lib/catalog_request_list");
+const Catalog_Delete = require("../../src/lib/catalog_request_delete.stub");
 
 const list = new Catalog_List();
+const del = new Catalog_Delete();
+
+// let deleteMe = "3JE3LPA4YCOKPQQ4OIKYWLRK";
+// let deleteMe2 = "ZWNCFJ6PNYFVJNIETRNFXWPM";
+let deleteMe2 = "not_even_a_real_id";
+
+// del.object_ids = deleteMe;
+del.nix(deleteMe2);
+
+console.log(del.fardel);
 
 const log = async function () {
   await list.request();
   console.log(list.fardel);
   console.log(list.delivery);
+
+  await del.request();
+  console.log(del.delivery);
 };
 
 log();
