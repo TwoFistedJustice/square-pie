ab230d23dd6fb0b4f092529b561ac71ce5b6bdb6
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-09-14 14:24:36 -0700

    test: implement unit tests for Catalog_Search_Cross_Reference

--- tests/catalog_request.test.js
+++ tests/catalog_request.test.js
@@ -313,3 +313,84 @@ describe("Catalog Request Search Filter", () => {
     expect(filter.object_types).toEqual(expected);
   });
 });
+
+describe("Catalog_Search_Cross_Reference", () => {
+  const Catalog_Search_Cross_Reference = require("../src/lib/stub.catalog_request_search_objects_cross_reference");
+  let id1 = "HXUTLPOIUE3FZGSK4NBZGMZD";
+  let id2 = "RJREZRB5H4RVFUMXB3R5V73Z";
+  // test("", () => {});
+
+  test(" variations method correctly modifies body", () => {
+    let expected = {
+      cursor: undefined,
+      include_related_objects: undefined,
+      begin_time: undefined,
+      object_types: undefined,
+      query: undefined,
+      item_variations_for_item_option_values_query: [],
+    };
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.items();
+    xref.variations();
+    expect(xref.body).toMatchObject(expected);
+  });
+
+  test("items method correctly modifies  body", () => {
+    let expected = {
+      cursor: undefined,
+      include_related_objects: undefined,
+      begin_time: undefined,
+      object_types: undefined,
+      query: undefined,
+      items_for_item_options_query: [],
+    };
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.variations();
+    xref.items();
+    expect(xref.body).toMatchObject(expected);
+  });
+
+  test("modifiers method correctly modifies  body", () => {
+    let expected = {
+      cursor: undefined,
+      include_related_objects: undefined,
+      begin_time: undefined,
+      object_types: undefined,
+      query: undefined,
+      items_for_modifier_list_query: [],
+    };
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.items();
+    xref.modifiers();
+    expect(xref.body).toMatchObject(expected);
+  });
+
+  test("taxes correctly modifies  body", () => {
+    let expected = {
+      cursor: undefined,
+      include_related_objects: undefined,
+      begin_time: undefined,
+      object_types: undefined,
+      query: undefined,
+      items_for_tax_query: [],
+    };
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.items();
+    xref.taxes();
+    expect(xref.body).toMatchObject(expected);
+  });
+
+  test("addId method adds ids", () => {
+    let expected = [id1, id2];
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.addId(id1).addId(id2);
+    expect(xref.ids).toMatchObject(expected);
+  });
+
+  test("clearIds clears the ids", () => {
+    let expected = [];
+    let xref = new Catalog_Search_Cross_Reference();
+    xref.addId(id1).addId(id2).clearIds();
+    expect(xref.ids).toMatchObject(expected);
+  });
+});
