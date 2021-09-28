dde5258cfa6c665ec366132170581a71756bdc4c
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-09-27 18:29:31 -0700

    tests: outline test stubs

--- tests/order_object.test.js
+++ tests/order_object.test.js
@@ -8,15 +8,38 @@ describe("Silence order object tests", () => {
 });
 
 describe("Order object build_discount method", () => {
-  // name should respect length limit
-  // catalog_object_id should respect length limit
-  // build_discount should set up correctly
-  // scope_line
-  // scope_order
-  //type_percentage
-  // type_amount
-  // correctly sets uid to lower-dashed name if no uid present
-  // throws if has neither type nor catalog_object_id
-  // amount_money
-  // applied_money
+  const Order_Object = require("../src/lib/stub.order_object");
+
+  test.only("pricing_options setter should throw if object not correctly formatted", () => {
+    let expected = [
+      {
+        auto_apply_discounts: true,
+        auto_apply_taxes: false,
+      },
+    ];
+    let order = new Order_Object();
+    order.pricing(true, false);
+    expect(order.pricing_options).toMatchObject(expected);
+  });
+
+  // DONE pricing_options setter should throw if object not correctly formatted
+  // pricing_options setter should throw if object doesn't have two booleans
+  // pricing_options setter should NOT throw if object is correctly formatted
+
+  // build_state methods should set state property as expected - there are four of them
+
+  //build_service_charge_amount should correctly build an 'amount_money' object when given two args
+  //build_service_charge_amount should automatically set "USD" when given just amount
+
+  //build_service_charge_applied should correctly build an 'amount_money' object when given two args
+  //build_service_charge_applied should automatically set "USD" when given just amount
+
+  // build_discount should do all the things - like 9 things minimum - have fun
+  // build_discount.name should respect length limit
+
+  //build_discount.percentage should throw if fed an arg that can't convert to a number
+  //build_discount.percentage should NOT throw if fed a compatible string
+  //build_discount.percentage should NOT throw if fed a number
+
+  // pricing should correctly format the output
 });
