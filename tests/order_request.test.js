7a2473432cbb03896c0deed3eac90d9344f4c69f
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-10-10 17:39:22 -0700

    test: implement test for Order_Request_Create

--- tests/order_request.test.js
+++ tests/order_request.test.js
@@ -16,12 +16,26 @@ describe("Order Request Body formatting", () => {
     // todo add an order object and test that it is properly attached
     let create = new Order_Create();
     let order = new Order_Object();
-    order.build_discount().type_amount().uid(name).add();
+    let uuid = create.idempotency_key;
+    order.build_discount().type_amount().uid("Pieville USA").add();
     create.body = order.fardel;
-
     let body = create.body;
+
+    let expected = {
+      idempotency_key: uuid,
+      order: {
+        discounts: [
+          {
+            type: "FIXED_AMOUNT",
+            uid: "Pieville USA",
+          },
+        ],
+      },
+    };
+
     Object.prototype.hasOwnProperty.call(body, "idempotency_key").should.be
       .true;
     Object.prototype.hasOwnProperty.call(body, "order").should.be.true;
+    expect(create.body).toMatchObject(expected);
   });
 });
