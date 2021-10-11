88d00767de23699b3206d1c573069190ef55aabe
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-10-10 17:00:13 -0700

    chore: rough out Order Create class

--- src/lib/stub.order_request_create.js
+++ src/lib/stub.order_request_create.js
@@ -1,21 +1,37 @@
 const Order_Request = require("./order_request");
+const { v4: uuidv4 } = require("uuid");
 
 class Order_Create extends Order_Request {
   constructor(props) {
     super(props);
     this._method = "post";
     this._endpoint = "";
-    this._body = {};
+
+    this._body = {
+      idempotency_key: uuidv4(),
+      order: undefined,
+    };
   }
 
   get body() {
     return this._body;
+  }
+  get idempotency_key() {
+    return this._body.idempotency_key;
+  }
+
+  set body(fardel) {
+    this._body.order = fardel;
   }
 
   make() {
     const methods = () => {
       let properties = {
         self: this,
+        body: function (fardel) {
+          this.body = fardel;
+          return this;
+        },
       };
       return properties;
     };
