6c24a1803a549f4f89b875f82fb68e603ab45544
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-10-11 17:56:47 -0700

    chore: wireframe Pay

--- src/lib/stub.order_request_pay.js
+++ src/lib/stub.order_request_pay.js
@@ -1,16 +1,30 @@
 const Order_Request = require("./order_request");
+const { v4: uuidv4 } = require("uuid");
 
 class Order_Pay extends Order_Request {
   constructor(props) {
     super(props);
     this._method = "post";
     this._endpoint = "";
-    this._body = {};
+    this._body = {
+      idempotency_key: uuidv4(),
+      order_version: undefined,
+      payments_ids: [],
+    };
   }
-
+  // GETTERS
   get body() {
     return this._body;
   }
+
+  // SETTERS
+
+  // TODO --  HANDY DANDY METHODS
+  // add payment id
+  // remove payment id by id
+  // add array of payment ids
+  // clear payment ids
+  //version
 
   make() {
     const methods = () => {
