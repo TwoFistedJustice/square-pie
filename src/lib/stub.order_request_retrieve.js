c97225e9a9a205c94218c59ecd8eea649918ebf0
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-10-11 17:32:06 -0700

    chore: frame most of retrieve

--- src/lib/stub.order_request_retrieve.js
+++ src/lib/stub.order_request_retrieve.js
@@ -8,7 +8,10 @@ class Order_Retrieve extends Order_Request {
     super(props);
     this._method = "post";
     this._endpoint = "batch-retrieve";
-    this._body = {};
+    this._body = {
+      location_id: undefined,
+      order_ids: [],
+    };
     this._delivery;
   }
 
@@ -19,15 +22,50 @@ class Order_Retrieve extends Order_Request {
   get delivery() {
     return this._delivery;
   }
-
+  get location_id() {
+    return this._body.location_id;
+  }
+  get order_ids() {
+    return this._body.order_ids;
+  }
+  set location_id(id) {
+    this._body.location_id = id;
+  }
+  set order_ids(id) {
+    this._body.order_ids.push(id);
+  }
   set delivery(parcel) {
     this._delivery = parcel.orders;
+  }
+  // todo order_array - take an array of ids and merge it
+  // todo order-remove by id
+  // todo order remove via pop
+  location(id) {
+    this.location = id;
+    return this;
+  }
+  clear_location() {
+    this.location = undefined;
+    return this;
+  }
+
+  add_order(id) {
+    this.order_ids = id;
+    return this;
   }
 
   make() {
     const methods = () => {
       let properties = {
         self: this,
+        location_id: function (id) {
+          this.location_id = id;
+          return this;
+        },
+        order_ids: function (id) {
+          this.order_ids = id;
+          return this;
+        },
       };
       return properties;
     };
