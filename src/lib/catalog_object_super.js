096eb95191e7324d47c02d11f3182b33be3030f6
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-11-02 15:19:34 -0700

    chore: add todo for refactor
    class should be refactored to use the maxLength utility instead of own mehtod

--- src/lib/catalog_object_super.js
+++ src/lib/catalog_object_super.js
@@ -1,97 +1,100 @@
 // This class should own the "ID" value and automatically insert the required '#' character if the user does not provide it.
 // (check if the first character is a hash, and insert one if it's not there)
 
-/** Base class for the Square catalog API 
- * 
+/** Base class for the Square catalog API
+ *
  * This class should own the "ID" value and automatically insert the required '#' character if the user does not provide it.
  * (check if the first character is a hash, and insert one if it's not there)
-*/
+ */
 class Catalog_Object_Super {
-	/**
-  * Create a catalog object
-  */
-	constructor() {
-		// do not add a constructor argument as it will interfere with subclasses
-		this._fardel = {
-			id                       : undefined,
-			present_at_all_locations : undefined, // bool
-			present_at_location_ids  : undefined //[str]
-		};
-	}
-	/**
+  /**
+   * Create a catalog object
+   */
+  constructor() {
+    // do not add a constructor argument as it will interfere with subclasses
+    this._fardel = {
+      id: undefined,
+      present_at_all_locations: undefined, // bool
+      present_at_location_ids: undefined, //[str]
+    };
+  }
+  /**
    * Gets catalog object id
    * @returns {string} id
    */
-	get id() {
-		return this._fardel.id;
-	}
+  get id() {
+    return this._fardel.id;
+  }
 
-	/**
+  /**
    * Gets if object can be found at all locations
    * @returns {boolean}
    */
-	get present_at_all_locations() {
-		return this._fardel.present_at_all_locations;
-	}
+  get present_at_all_locations() {
+    return this._fardel.present_at_all_locations;
+  }
 
-	/**
-  * Gets list of ids for locations that have object
-  * @returns {(string[])} of location ids
-  */
-	get present_at_location_ids() {
-		return this._fardel.present_at_location_ids;
-	}
+  /**
+   * Gets list of ids for locations that have object
+   * @returns {(string[])} of location ids
+   */
+  get present_at_location_ids() {
+    return this._fardel.present_at_location_ids;
+  }
 
-	/**
+  /**
    * Sets the catalog object id
    * @param {string} tempId
    * @returns {string} the new id
    */
-	set id(tempId) {
-		if (tempId[0] !== '#') {
-			this._fardel.id = '#' + tempId.slice(0);
-		} else {
-			this._fardel.id = tempId;
-		}
-	}
+  set id(tempId) {
+    if (tempId[0] !== "#") {
+      this._fardel.id = "#" + tempId.slice(0);
+    } else {
+      this._fardel.id = tempId;
+    }
+  }
 
-	/**
-  * Sets if object can be found at all locations
-	* 
-  * @param {boolean} bool
-  * @returns {boolean} true or false 
-  */
-	set present_at_all_locations(bool) {
-		this._fardel.present_at_all_locations = bool;
-	}
+  /**
+   * Sets if object can be found at all locations
+   *
+   * @param {boolean} bool
+   * @returns {boolean} true or false
+   */
+  set present_at_all_locations(bool) {
+    this._fardel.present_at_all_locations = bool;
+  }
 
-	/**
-  * Sets locations by id of were the object is stocked
-	* 
-  * @param {string[]} id
-  * @returns {string[]} list of location ids
-  */
-	set present_at_location_ids(id) {
-		if (!Array.isArray(this.present_at_location_ids)) {
-			this._fardel.present_at_location_ids = [];
-		}
-		this._fardel.present_at_location_ids.push(id);
-	}
+  /**
+   * Sets locations by id of were the object is stocked
+   *
+   * @param {string[]} id
+   * @returns {string[]} list of location ids
+   */
+  set present_at_location_ids(id) {
+    if (!Array.isArray(this.present_at_location_ids)) {
+      this._fardel.present_at_location_ids = [];
+    }
+    this._fardel.present_at_location_ids.push(id);
+  }
 
-	// METHODS
-	/**
-  * Sets max character length and throws error if surpassed
-	* 
-  * @param {number} max - max character length
-  * @param {string} [str=''] - string to be tested. default is ''
-  * @returns {boolean} if string is under max length
-  */
-	maxLength(max, str = '') {
-		if (str.length > max) {
-			throw new Error(`Surpassed maximum character limit of ${max}.\n${str}`);
-		}
-		return true;
-	}
+  // METHODS
+  /**
+   * Sets max character length and throws error if surpassed
+   *
+   * @param {number} max - max character length
+   * @param {string} [str=''] - string to be tested. default is ''
+   * @returns {boolean} if string is under max length
+   */
+
+  // todo refactor this to use the utility
+  //  require it at the top and remove this method
+  maxLength(max, str = "") {
+    if (str.length > max) {
+      throw new Error(`Surpassed maximum character limit of ${max}.\n${str}`);
+    }
+    return true;
+  }
 } // END class
 
 module.exports = Catalog_Object_Super;
