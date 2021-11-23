faee98f9d2d56736a93a78c48aaad7e9f3b4d58b
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-11-22 17:56:54 -0800

    refactor: swap in define(), call super enum_object_types from make()

--- src/lib/catalog_request_search_objects_cross_reference.js
+++ src/lib/catalog_request_search_objects_cross_reference.js
@@ -1,5 +1,6 @@
 const Catalog_Search_Objects_Super = require("./catalog_request_search_objects_super");
-const { setter_chain_generator_config } = require("./utilities");
+const { define } = require("./utilities");
+
 class Catalog_Search_Cross_Reference extends Catalog_Search_Objects_Super {
   #_ids;
   constructor() {
@@ -20,42 +21,26 @@ class Catalog_Search_Cross_Reference extends Catalog_Search_Objects_Super {
 
   variations() {
     this.queryRemove();
-    Object.defineProperty(
+    define(
       this._body,
       "item_variations_for_item_option_values_query",
-      {
-        value: this.ids,
-        writable: true,
-        enumerable: true,
-      }
+      this.ids
     );
     return this;
   }
   items() {
     this.queryRemove();
-    Object.defineProperty(this._body, "items_for_item_options_query", {
-      value: this.ids,
-      writable: true,
-      enumerable: true,
-    });
+    define(this._body, "items_for_item_options_query", this.ids);
     return this;
   }
   modifiers() {
     this.queryRemove();
-    Object.defineProperty(this._body, "items_for_modifier_list_query", {
-      value: this.ids,
-      writable: true,
-      enumerable: true,
-    });
+    define(this._body, "items_for_modifier_list_query", this.ids);
     return this;
   }
   taxes() {
     this.queryRemove();
-    Object.defineProperty(this._body, "items_for_tax_query", {
-      value: this.ids,
-      writable: true,
-      enumerable: true,
-    });
+    define(this._body, "items_for_tax_query", this.ids);
     return this;
   }
 
@@ -70,23 +55,21 @@ class Catalog_Search_Cross_Reference extends Catalog_Search_Objects_Super {
   }
 
   make() {
-    const methods = () => {
-      // any changes made to super modification methods should be replicated on Catalog_Search_Filter
-      const properties = {
-        self: this,
-        include_related_objects: function (bool) {
-          this.self.include_related_objects = bool;
-          return this;
-        },
-        begin_time: function (time) {
-          this.self.begin_time = time;
-          return this;
-        },
-      };
-      setter_chain_generator_config(this.configuration, properties, this);
-      return properties;
+    // any changes made to super modification methods should be replicated on Catalog_Search_Filter
+    return {
+      self: this,
+      include_related_objects: function (bool) {
+        this.self.include_related_objects = bool;
+        return this;
+      },
+      begin_time: function (time) {
+        this.begin_time = time;
+        return this;
+      },
+      object_types: function () {
+        return this.self.enum_object_types();
+      },
     };
-    return methods();
   }
 }
 module.exports = Catalog_Search_Cross_Reference;
