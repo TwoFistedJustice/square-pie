9db9b53f8afde415b3c206d24e4aee2e431b503a
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-04 18:08:30 -0800

    refactor: move utility into own file: define()

--- src/lib/utilities/aaa_index.js
+++ src/lib/utilities/aaa_index.js
@@ -1,25 +1,6 @@
 const { isISO4217, isRFC3339 } = require("validator");
 const normalize_email = require("./normalize_email");
-
-/** Use define() for building those lovely complex objects that Square likes in the request body.
- *
- *  creates new properties that are configurable, enumerable, and writable.
- *
- * @param {object} object_to_modify is a reference to the object you want to modify
- * @param {string} prop is the name of the property you want to add
- * @param {any} val can be pretty much any valid javascript value
- * @return mutates the object you pass in as the first argument
- *
- * (It's an arrow function on purpose - don't change it)
- * */
-const define = (object_to_modify, prop, val) => {
-  Object.defineProperty(object_to_modify, prop, {
-    value: val,
-    configurable: true,
-    enumerable: true,
-    writable: true,
-  });
-};
+const define = require("./define");
 
 /*
  *  may need to be refactored for 'this' inside the class
