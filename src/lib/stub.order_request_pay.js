7c99e1f00ef6029d9ce35bebd7fce8aefffab58d
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-08 17:26:47 -0800

    feat: wireframe Order_Clone

--- src/lib/stub.order_request_pay.js
+++ src/lib/stub.order_request_pay.js
@@ -4,10 +4,10 @@ const { nanoid } = require("nanoid");
 class Order_Pay extends Order_Request {
   _display_name = "Order_Pay";
   _last_verified_square_api_version = "2021-07-21";
-  constructor(props) {
-    super(props);
+  constructor(id) {
+    super();
     this._method = "post";
-    this._endpoint = "";
+    this._endpoint = `/${id}/pay`;
     this._body = {
       idempotency_key: nanoid(),
       order_version: undefined,
@@ -25,18 +25,45 @@ class Order_Pay extends Order_Request {
     return this._body;
   }
 
-  // SETTERS
+  // GETTERS
+  get idempotency_key() {
+    return this._body.idempotency_key;
+  }
+  get order_version() {
+    return this._body.order_version;
+  }
+  get payment_ids() {
+    return this._body.payment_ids;
+  }
 
-  // TODO --  MAKER METHODS
-  // add payment id
-  // remove payment id by id
-  // add array of payment ids
-  // clear payment ids
-  //version
+  // SETTERS
+  set idempotency_key(val) {
+    this._body.idempotency_key = val;
+  }
+  set order_version(val) {
+    this._body.order_version = val;
+  }
+  set payment_ids(val) {
+    this._body.payment_id = val;
+  }
+
+  // MAKER METHODS
 
   make() {
     return {
       self: this,
+      idempotency_key: function (val) {
+        this.self.idempotency_key = val;
+        return this;
+      },
+      order_version: function (val) {
+        this.self.order_version = val;
+        return this;
+      },
+      payment_ids: function (val) {
+        this.self.payment_ids = val;
+        return this;
+      },
     };
   }
 }
