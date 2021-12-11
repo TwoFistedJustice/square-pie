99ace25dc9409135218b056ef1a33c4a86aea376
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-11 12:27:54 -0800

    refactor: extract enum out of line_item to own file
    will be needed for Order_Search query

---
+++ src/lib/enum/order_line_item_enum.js
@@ -0,0 +1,45 @@
+/**
+ * Forget the mixin docs and examples online. They don't work with class 'this'
+ * passing 'this' as an argument works without counter-intuitive syntax
+ * */
+
+const order_line_item_item_type = {
+  /** @enum  an array of the allowable values on a property.
+   * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
+   * @example
+   *
+   * Use in make() for preventing the class setter from passing disallowed values
+   * Do not use in the setter itself because it will be called every time
+   * */
+
+  allowable_values: ["ITEM", "CUSTOM_AMOUNT"],
+
+  /** @function enum
+   * @enum  enables a referencing class to set only allowable values on a property.
+   * @param {object} self - pass in 'this' from the referencing class.
+   * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
+   * @example
+   * #enum_PROPERTY_NAME() {
+   *   return Name_Of_Class_property.enum(this)
+   * }
+   * */
+
+  enum: function (self) {
+    return {
+      item: function () {
+        self.item = "ITEM";
+        return this;
+      },
+      custom: function () {
+        self.item = "CUSTOM_AMOUNT";
+        return this;
+      },
+      gift: function () {
+        self.item = "GIFT_CARD";
+        return this;
+      },
+    };
+  },
+};
+
+module.exports = order_line_item_item_type;
