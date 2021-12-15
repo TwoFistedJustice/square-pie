46f5ed243fb9cdcb695ff15d9d5fdade08154e6c
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-14 17:04:10 -0800

    feat: wireframe Invoice_Request class

---
+++ src/lib/stub.invoice_request_abstract.js
@@ -0,0 +1,23 @@
+const Square_Request = require("./square_request_abstract");
+
+class Invoice_Request extends Square_Request {
+  _display_name = "Invoice_Request";
+  constructor() {
+    super();
+    this._api_name = "invoices";
+    this._delivery;
+  }
+
+  get display_name() {
+    return this._display_name;
+  }
+
+  get delivery() {
+    return this._delivery;
+  }
+
+  set delivery(parcel) {
+    this._delivery = parcel.invoice;
+  }
+}
+module.exports = Invoice_Request;
