0719fed7c7e4e6ba8e76d24447eedaf8737fe234
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-15 18:50:30 -0800

    chore: add empty file for invoice object

--- src/lib/invoice_request_create.js
+++ src/lib/invoice_request_create.js
@@ -1,5 +1,6 @@
 const Invoice_Request = require("./stub.invoice_request_abstract");
-const nanoid = require("nanoid");
+const { nanoid } = require("nanoid");
+const { shazam_max_length } = require("./utilities/aaa_index");
 
 /** @class  Invoice_Create
  * @param {}
@@ -17,6 +18,7 @@ class Invoice_Create extends Invoice_Request {
 
     this._body = {
       idempotency_key: nanoid(), // 128
+      invoice: undefined,
     };
     this.configuration = {
       maximums: {
@@ -24,18 +26,51 @@ class Invoice_Create extends Invoice_Request {
       },
     };
   }
-
+  // GETTERS
   get display_name() {
     return this._display_name;
   }
   get square_version() {
     return `The last verified compatible Square API version is ${this._last_verified_square_api_version}`;
   }
+  get endpoint() {
+    return this._endpoint;
+  }
+
+  get body() {
+    return this._body;
+  }
+  get idempotency_key() {
+    return this._body.idempotency_key;
+  }
+
+  // SETTERS
+
+  set idempotency_key(key) {
+    if (
+      shazam_max_length(
+        this.configuration.maximums.idempotency_key,
+        key,
+        this.display_name,
+        "idempotency_key"
+      )
+    ) {
+      this._body.idempotency_key = key;
+    }
+  }
 
   // MAKER METHODS
   make() {
     return {
       self: this,
+      idempotency_key: function (key) {
+        this.self.idempotency_key = key;
+        return this;
+      },
+      invoice: function (order) {
+        this.self.invoice = order;
+        return this;
+      },
     };
   }
 }
