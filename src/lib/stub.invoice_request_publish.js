110e46679f4b7f253266002e4af9a28e84617cbe
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-16 19:17:09 -0800

    feat: implement Order_Cancel and Order_Publish

--- src/lib/stub.invoice_request_publish.js
+++ src/lib/stub.invoice_request_publish.js
@@ -1,5 +1,6 @@
 const Invoice_RUDCnP = require("./stub.invoice_request_abstract_RUDCP_super");
-const nanoid = require("nanoid");
+const { nanoid } = require("nanoid");
+const { shazam_integer, shazam_max_length } = require("./utilities/aaa_index");
 
 /** @class Invoice_Publish
  * @param {string} id The invoice_id of the invoice you want
@@ -9,13 +10,15 @@ const nanoid = require("nanoid");
 class Invoice_Publish extends Invoice_RUDCnP {
   _display_name = "Invoice_Publish";
   _last_verified_square_api_version = "2021-11-17";
+  _help =
+    "You must provide the invoice VERSION. If you do not know the version, call Invoice_Retrieve or Invoice_List.\nYou can add the invoice_id either as an argument when yuo instantiate the class, calling the setter, or calling make().id()";
   constructor(id) {
-    super(id);
+    super();
     this._method = "POST";
     this._endpoint = `/${id}/publish`;
     this._body = {
-      idempotency_key: nanoid(), // 128
-      version: undefined, //int32
+      idempotency_key: nanoid(),
+      version: undefined, //int32 REQUIRED
     };
     this.configuration = {
       maximums: {
@@ -23,29 +26,66 @@ class Invoice_Publish extends Invoice_RUDCnP {
       },
     };
   }
-
+  // GETTERS
   get display_name() {
     return this._display_name;
   }
   get square_version() {
     return `The last verified compatible Square API version is ${this._last_verified_square_api_version}`;
   }
-  get delivery() {
-    return this._delivery;
-  }
 
   get id() {
     return this._endpoint;
   }
+  get version() {
+    return this._body.version;
+  }
+
+  get help() {
+    return this._help;
+  }
+
+  get idempotency_key() {
+    return this._body.idempotency_key;
+  }
+
+  // SETTERS
+
+  set idempotency_key(key) {
+    if (
+      shazam_max_length(
+        this.configuration.maximums.idempotency_key,
+        key,
+        this.display_name,
+        "idempotency_key"
+      )
+    ) {
+      this._body.idempotency_key = key;
+    }
+  }
 
   set id(someId) {
-    this._endpoint = `/${someId}`;
+    this._endpoint = "/" + someId + "/publish";
+  }
+
+  set version(ver) {
+    if (shazam_integer(ver, this._display_name, "version")) {
+      this._body.version = ver;
+    }
   }
 
   // MAKER METHODS
   make() {
     return {
       self: this,
+      id: function (id) {
+        this.self.id = id;
+        return this;
+      },
+      version: function (ver) {
+        this.self.version = ver;
+        return this;
+      },
     };
   }
 }
--- tests/invoice_request_structures.test.js
+++ tests/invoice_request_structures.test.js
@@ -2,6 +2,8 @@
 const Invoice_Create = require("../src/lib/invoice_request_create");
 const Invoice_Retrieve = require("../src/lib/stub.invoice_request_retrieve");
 const Invoice_Delete = require("../src/lib/stub.invoice_request_delete");
+const Invoice_Cancel = require("../src/lib/stub.invoice_request_cancel");
+const Invoice_Publish = require("../src/lib/stub.invoice_request_publish");
 
 const { long_strings } = require("./helper_objects");
 
@@ -11,6 +13,11 @@ describe("silence test suite", () => {
   });
 });
 
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Invoice_Create
+ *                                                         *
+ * ------------------------------------------------------- */
 describe("Invoice_Create", () => {
   let create;
   beforeEach(function () {
@@ -60,6 +67,11 @@ describe("Invoice_Create", () => {
   });
 });
 
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Invoice_Retrieve
+ *                                                         *
+ * ------------------------------------------------------- */
 describe("Invoice_Retrieve", () => {
   let retrieve;
   let id = "123";
@@ -97,6 +109,11 @@ describe("Invoice_Retrieve", () => {
   });
 });
 
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Invoice_Delete
+ *                                                         *
+ * ------------------------------------------------------- */
 describe("Invoice_Delete", () => {
   let del;
   let class_name = "Invoice_Delete";
@@ -138,5 +155,153 @@ describe("Invoice_Delete", () => {
   test("make().id() should set the property", () => {
     del.make().id(id2);
     expect(del.id).toEqual(endpoint2);
+  });
+});
+
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Invoice_Cancel
+ *                                                         *
+ * ------------------------------------------------------- */
+describe("Invoice_Cancel", () => {
+  let cancel;
+  let class_name = "Invoice_Cancel";
+  let id = "123";
+  let endpoint = `/${id}/cancel`;
+  let id2 = "ABC";
+  let endpoint2 = `/${id2}/cancel`;
+  beforeEach(function () {
+    cancel = new Invoice_Cancel(id);
+  });
+
+  test("should have display name", () => {
+    expect(cancel._display_name).toBeDefined();
+  });
+
+  test("should have the method defined by Square set", () => {
+    expect(cancel.method).toEqual("POST");
+  });
+
+  test("display name should be same as class name", () => {
+    expect(cancel._display_name).toEqual(class_name);
+  });
+
+  test("should have defined square version", () => {
+    expect(cancel.square_version).toBeDefined();
+  });
+  test("should have defined _body", () => {
+    expect(cancel.body).toBeDefined();
+  });
+
+  test("should have _delivery", () => {
+    let expected = { a: 1 };
+    cancel.delivery = { invoice: { a: 1 } };
+    expect(cancel.delivery).toMatchObject(expected);
+  });
+
+  test("should have an endpoint", () => {
+    expect(cancel.endpoint).toEqual(endpoint);
+  });
+
+  // Make()
+  test("make().id() should set the endpoint property", () => {
+    cancel.make().id(id2);
+    expect(cancel.id).toEqual(endpoint2);
+    expect(cancel.endpoint).toEqual(endpoint2);
+  });
+
+  test("make().version() should set version property", () => {
+    let expected = 1;
+    cancel.make().version(expected);
+    expect(cancel.version).toEqual(expected);
+  });
+  // version should throw if not an int
+
+  test("version should throw if not an int", () => {
+    expect(() => {
+      cancel.version = 95.5;
+    }).toThrow();
+  });
+});
+
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Invoice_Publish
+ *                                                         *
+ * ------------------------------------------------------- */
+
+describe("Invoice_Publish", () => {
+  let publish;
+  let class_name = "Invoice_Publish";
+  let id = "123";
+  let endpoint = `/${id}/publish`;
+  let id2 = "ABC";
+  let endpoint2 = `/${id2}/publish`;
+  beforeEach(function () {
+    publish = new Invoice_Publish(id);
+  });
+
+  test("should have defined _body.idempotency_key", () => {
+    expect(publish.body.idempotency_key).toBeDefined();
+  });
+
+  test("nanoid should generate a idempotency key less than the limit", () => {
+    let pass = publish.idempotency_key.length <= 128;
+    expect(pass).toEqual(true);
+  });
+
+  test("idempotency should respect length 128", () => {
+    expect(() => {
+      publish.idempotency_key = long_strings.len_129;
+    }).toThrow();
+  });
+
+  test("should have display name", () => {
+    expect(publish._display_name).toBeDefined();
+  });
+
+  test("should have the method defined by Square set", () => {
+    expect(publish.method).toEqual("POST");
+  });
+
+  test("display name should be same as class name", () => {
+    expect(publish._display_name).toEqual(class_name);
+  });
+
+  test("should have defined square version", () => {
+    expect(publish.square_version).toBeDefined();
+  });
+  test("should have defined _body", () => {
+    expect(publish.body).toBeDefined();
+  });
+
+  test("should have _delivery", () => {
+    let expected = { a: 1 };
+    publish.delivery = { invoice: { a: 1 } };
+    expect(publish.delivery).toMatchObject(expected);
+  });
+
+  test("should have an endpoint", () => {
+    expect(publish.endpoint).toEqual(endpoint);
+  });
+
+  // Make()
+  test("make().id() should set the endpoint property", () => {
+    publish.make().id(id2);
+    expect(publish.id).toEqual(endpoint2);
+    expect(publish.endpoint).toEqual(endpoint2);
+  });
+
+  test("make().version() should set version property", () => {
+    let expected = 1;
+    publish.make().version(expected);
+    expect(publish.version).toEqual(expected);
+  });
+  // version should throw if not an int
+
+  test("version should throw if not an int", () => {
+    expect(() => {
+      publish.version = 95.5;
+    }).toThrow();
   });
 });
