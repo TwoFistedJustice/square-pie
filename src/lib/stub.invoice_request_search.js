1b4bf8e37d71125192a182d77e3be2ffe9fc94f2
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-17 16:43:58 -0800

    chore: partial rough draft Invoice_Search

--- src/lib/stub.invoice_request_search.js
+++ src/lib/stub.invoice_request_search.js
@@ -1,4 +1,5 @@
 const Invoice_Request = require("./stub.invoice_request_abstract");
+const { shazam_integer, shazam_number_LE } = require("./utilities/aaa_index");
 
 /** @class  Invoice_Search
  * @param {}
@@ -8,40 +9,85 @@ const Invoice_Request = require("./stub.invoice_request_abstract");
 class Invoice_Search extends Invoice_Request {
   _display_name = "Invoice_Search";
   _last_verified_square_api_version = "2021-11-17";
+  _help =
+    "Build a query using the build_query() method. Only call the setter or make().query() if you are passing a fully formed query object as it will replace everything." +
+    "\nLimit has a default of 100 and max of 200.";
   constructor() {
     super();
 
     this._method = "POST";
     this._endpoint = "/search";
-    this._delivery = undefined;
+    this._delivery = [];
 
-    this._body = {};
+    this._body = {
+      query: {},
+      limit: undefined, // int max 200, default 100
+      cursor: undefined, // gets set by Square Request
+    };
+
     this.configuration = {
       maximums: {
-        idempotency_key: 128,
+        limit: 200,
       },
     };
   }
-
+  // GETTERS
   get display_name() {
     return this._display_name;
   }
+
+  get help() {
+    return this._help;
+  }
+
   get square_version() {
     return `The last verified compatible Square API version is ${this._last_verified_square_api_version}`;
   }
-
   get delivery() {
     return this._delivery;
   }
 
+  get query() {
+    return this._body.query;
+  }
+  get limit() {
+    return this._body.limit;
+  }
+  // SETTERS
+
+  set query(val) {
+    this._body.query = val;
+  }
+  set limit(int) {
+    let name = this.display_name;
+    let caller = "limit";
+    if (
+      shazam_integer(int, name, caller) &&
+      shazam_number_LE(int, this.configuration.maximums.limit, name, caller)
+    ) {
+      this._body.limit = int;
+    }
+  }
+
   set delivery(parcel) {
-    this._delivery = parcel.invoices;
+    if (Object.prototype.hasOwnProperty.call(parcel, "cursor")) {
+      this._body.cursor = parcel.cursor;
+    }
+    this._delivery.push(parcel.invoices);
   }
 
   // MAKER METHODS
   make() {
     return {
       self: this,
+      limit: function (val) {
+        this.self.limit = val;
+        return this;
+      },
+      query: function (val) {
+        this.self.query = val;
+        return this;
+      },
     };
   }
 }
