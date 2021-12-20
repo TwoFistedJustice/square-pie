e316994dfc59492ef86f24d9014dd73b9cd618b3
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-20 15:52:39 -0800

    feat: truncate strings longer than 255 in error messages
    very longs strings bog down the program and could clutters up logs
    with huge blocks of text. 255 is the maximum Sqaure allows for document
    ids. So that's what we went wtih.

--- src/lib/utilities/shazam_max_length.js
+++ src/lib/utilities/shazam_max_length.js
@@ -2,7 +2,8 @@
 Returns true if the string is less than or equal to the max length
 * */
 /** shazam_max_length validates string length, returning true if the string is equal to or less than
- * the maximum allowable length and otherwise throwing an error.
+ * the maximum allowable length and otherwise throwing an error. If the string being checked exceeds 255
+ * characters and it triggers an error, it will be truncated to the first 255 characters within the error message.
  *
  * @param {number} max is the upper limit of allowable string length
  * @param {string} str is the string you want to validate
@@ -22,8 +23,15 @@ const shazam_max_length = function (
   caller = "- unspecified class setter"
 ) {
   if (str.length > max) {
+    let replacement_string;
+    if (str.length > 255) {
+      replacement_string = str.slice(0, 255) + "...";
+    } else {
+      replacement_string = str;
+    }
+
     throw new Error(
-      `${displayName}.${caller} - surpassed maximum character limit of ${max}.\nReceived: ${str}`
+      `${displayName}.${caller} - surpassed maximum character limit of ${max}.\nReceived: ${replacement_string}`
     );
   }
   return true;
