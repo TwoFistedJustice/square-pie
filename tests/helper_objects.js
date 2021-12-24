330e0584e6f19c0070103c80f429b2ca88e55056
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2021-12-23 18:11:42 -0800

    test: add invoice helper object
    copied from Square docs

--- tests/helper_objects.js
+++ tests/helper_objects.js
@@ -360,9 +360,72 @@ const sampleCustomers = {
   },
 };
 
+const invoice = {
+  id: "inv:0-ChCHu2mZEabLeeHahQnXDjZQECY",
+  version: 2,
+  location_id: "ES0RJRZYEC39A",
+  order_id: "CAISENgvlJ6jLWAzERDzjyHVybY",
+  payment_requests: [
+    {
+      uid: "2da7964f-f3d2-4f43-81e8-5aa220bf3355",
+      request_type: "BALANCE",
+      due_date: "2030-01-24",
+      tipping_enabled: false,
+      computed_amount_money: {
+        amount: 10000,
+        currency: "USD",
+      },
+      total_completed_amount_money: {
+        amount: 0,
+        currency: "USD",
+      },
+      automatic_payment_source: "NONE",
+    },
+  ],
+  invoice_number: "inv-100",
+  title: "Event Planning Services",
+  description: "We appreciate your business!",
+  scheduled_at: "2030-01-13T10:00:00Z",
+  status: "UNPAID",
+  timezone: "America/Los_Angeles",
+  created_at: "2020-06-18T17:45:13Z",
+  updated_at: "2020-06-18T18:23:11Z",
+  primary_recipient: {
+    customer_id: "JDKYHBWT1D4F8MFH63DBMEN8Y4",
+    given_name: "Amelia",
+    family_name: "Earhart",
+    email_address: "Amelia.Earhart@example.com",
+    phone_number: "1-212-555-4240",
+  },
+  next_payment_amount_money: {
+    amount: 10000,
+    currency: "USD",
+  },
+  accepted_payment_methods: {
+    card: true,
+    square_gift_card: false,
+    bank_account: false,
+  },
+  custom_fields: [
+    {
+      label: "Event Reference Number",
+      value: "Ref. #1234",
+      placement: "ABOVE_LINE_ITEMS",
+    },
+    {
+      label: "Terms of Service",
+      value: "The terms of service are...",
+      placement: "BELOW_LINE_ITEMS",
+    },
+  ],
+  delivery_method: "EMAIL",
+  sale_or_service_date: "2030-01-24",
+};
+
 module.exports = {
   long_strings,
   long_arrays,
   dateCodes,
   sampleCustomers,
+  invoice,
 };
