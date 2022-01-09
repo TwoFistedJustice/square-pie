22295923935791e83a22720abc1251d9b53f3e9b
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-01-09 14:35:23 -0800

    test: implement passing basic structure tests for Customer_Update: status:yellow

--- tests/stub.customer_request_update.test.js
+++ tests/stub.customer_request_update.test.js
@@ -1,4 +1,4 @@
-// const Customer_Update = require("../src/lib/customer_request_update");
+const Customer_Update = require("../src/lib/customer_request_update");
 
 // const Customer_Object = require("../src/lib/customer_object");
 // const { sampleCustomers } = require("./helper_objects");
@@ -18,14 +18,53 @@ describe("Silence test suite", () => {
  *                                                         *
  * ------------------------------------------------------- */
 
-// const class_name = "Customer_Update";
-// describe("", () => {
-//   let myVar;
-//   let endpoint = "";
-//
-//   beforeEach(() => {
-//     myVar = new Customer_Update();
-//
-//   });
-//
-// });
+describe("Customer_Update", () => {
+  let myVar;
+  let arg = "123";
+  let class_name = "Customer_Update";
+  let endpoint = `/${arg}`;
+  let method = "PUT"; //http method from Square docs
+  beforeEach(function () {
+    myVar = new Customer_Update(arg);
+  });
+
+  test("should have display name", () => {
+    expect(myVar._display_name).toBeDefined();
+  });
+  test("should have the method defined by Square set", () => {
+    expect(myVar.method).toEqual(method);
+  });
+  test("display name should be same as class name", () => {
+    expect(myVar.display_name).toEqual(class_name);
+  });
+  test("should have defined square version", () => {
+    expect(myVar.square_version).toBeDefined();
+  });
+  test("should have defined _help", () => {
+    expect(myVar.help).toBeDefined();
+  });
+  test("should have an endpoint", () => {
+    expect(myVar.endpoint).toEqual(endpoint);
+  });
+  test("should have _delivery", () => {
+    myVar.delivery = { customer: { a: 1 } };
+    expect(myVar.delivery).toBeDefined();
+  });
+
+  // not every request class has these
+  test("should have defined _body", () => {
+    expect(myVar.body).toBeDefined();
+  });
+  // test ("should have defined _body.idempotency_key", () => {
+  //   expect (myVar.body.idempotency_key).toBeDefined ();
+  // });
+  // test ("nanoid should generate a idempotency key less than the limit", () => {
+  //   let pass = myVar.idempotency_key.length <= 128;
+  //   expect (pass).toEqual (true);
+  // });
+  // test ("idempotency should respect length 192", () => {
+  //   expect (() => {
+  //     myVar.idempotency_key = long_strings.len_193;
+  //   }).toThrow ();
+  // });
+});
