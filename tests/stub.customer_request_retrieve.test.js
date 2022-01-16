44ad16651003be60372ae10d40cc93f5738efde6
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-01-15 17:55:51 -0800

    test: add basic structure tests

--- tests/stub.customer_request_retrieve.test.js
+++ tests/stub.customer_request_retrieve.test.js
@@ -1,4 +1,4 @@
-// const Customer_Retrieve = require("../src/lib/customer_request_retrieve");
+const Customer_Retrieve = require("../src/lib/customer_request_retrieve");
 
 // const Customer_Object = require("../src/lib/customer_object");
 // const { sampleCustomers } = require("./helper_objects");
@@ -17,15 +17,36 @@ describe("Silence test suite", () => {
  *                        Customer_Retrieve
  *                                                         *
  * ------------------------------------------------------- */
-// const class_name = "";
-// describe("", () => {
-//  let myVar;
-//  let endpoint = "";
-//
-//
-//   beforeEach(() => {
-//    myVar = new ();
-//
-//   });
-//
-// });
+describe("Customer_Retrieve", () => {
+  let retrieve;
+  let class_name = "Customer_Retrieve";
+  let endpoint = "/123"; //copy and paste from Square docs
+  let method = "GET"; //http method from Square docs
+  let id = "123";
+  beforeEach(function () {
+    retrieve = new Customer_Retrieve(id);
+  });
+
+  test("should have display name", () => {
+    expect(retrieve._display_name).toBeDefined();
+  });
+  test("should have the method defined by Square set", () => {
+    expect(retrieve.method).toEqual(method);
+  });
+  test("display name should be same as class name", () => {
+    expect(retrieve.display_name).toEqual(class_name);
+  });
+  test("should have defined square version", () => {
+    expect(retrieve.square_version).toBeDefined();
+  });
+  test("should have defined _help", () => {
+    expect(retrieve.help).toBeDefined();
+  });
+  test("should have an endpoint", () => {
+    expect(retrieve.endpoint).toEqual(endpoint);
+  });
+  test("should have _delivery", () => {
+    retrieve.delivery = { customer: { a: 1 } };
+    expect(retrieve.delivery).toBeDefined();
+  });
+});
--- tests/stub.customer_request_search.test.js
+++ tests/stub.customer_request_search.test.js
@@ -1,4 +1,4 @@
-// const Customer_Search = require("../src/lib/customer_request_search");
+const Customer_Search = require("../src/lib/customer_request_search");
 
 // const Customer_Object = require("../src/lib/customer_object");
 // const { sampleCustomers } = require("./helper_objects");
@@ -17,15 +17,40 @@ describe("Silence test suite", () => {
  *                        Customer_Search
  *                                                         *
  * ------------------------------------------------------- */
-// const class_name = "";
-// describe("", () => {
-//  let myVar;
-//  let endpoint = "";
-//
-//
-//   beforeEach(() => {
-//    myVar = new ();
-//
-//   });
-//
-// });
+describe("Customer_Search", () => {
+  let search;
+  let class_name = "Customer_Search";
+  let endpoint = "/search"; //copy and paste from Square docs
+  let method = "POST"; //http method from Square docs
+  beforeEach(function () {
+    search = new Customer_Search();
+  });
+
+  test("should have display name", () => {
+    expect(search._display_name).toBeDefined();
+  });
+  test("should have the method defined by Square set", () => {
+    expect(search.method).toEqual(method);
+  });
+  test("display name should be same as class name", () => {
+    expect(search.display_name).toEqual(class_name);
+  });
+  test("should have defined square version", () => {
+    expect(search.square_version).toBeDefined();
+  });
+  test("should have defined _help", () => {
+    expect(search.help).toBeDefined();
+  });
+  test("should have an endpoint", () => {
+    expect(search.endpoint).toEqual(endpoint);
+  });
+  test("should have _delivery", () => {
+    search.delivery = { customers: [{ a: 1 }] };
+    expect(search.delivery).toBeDefined();
+  });
+
+  // not every request class has these
+  test("should have defined _body", () => {
+    expect(search.body).toBeDefined();
+  });
+});
