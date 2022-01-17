3778a49a72fc30637daef53aeb9abe8f313ed646
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-01-16 18:00:36 -0800

    test: add unit tests for Square Request structures

---
+++ tests/square_request_abstract.test.js
@@ -0,0 +1,62 @@
+const Square_Request = require("../src/lib/square_request_abstract");
+
+describe("Square_Request", () => {
+  let square;
+  let class_name = "Square_Request";
+  let endpoint = ""; //copy and paste from Square docs
+  let method = ""; //http method from Square docs
+  beforeEach(function () {
+    square = new Square_Request();
+  });
+
+  test("should have display name", () => {
+    expect(square._display_name).toBeDefined();
+  });
+  test("should have the method defined by Square set", () => {
+    expect(square.method).toEqual(method);
+  });
+  test("display name should be same as class name", () => {
+    expect(square.display_name).toEqual(class_name);
+  });
+  test("should have defined square version", () => {
+    expect(square.square_version).toBeDefined();
+  });
+  test("should have defined _help", () => {
+    expect(square.help).toBeDefined();
+  });
+  test("should have an endpoint", () => {
+    expect(square.endpoint).toEqual(endpoint);
+  });
+  test("should have _delivery", () => {
+    square.delivery = { someProp: { a: 1 } };
+    expect(square.delivery).toBeDefined();
+  });
+
+  test("should have defined _secret", () => {
+    // expect (square.hasOwnProperty("_secret")).toEqual (true);
+    expect(Object.prototype.hasOwnProperty.call(square, "_secret")).toEqual(
+      true
+    );
+  });
+  test("Body should set", () => {
+    let expected = { a: 1 };
+    square.body = expected;
+    expect(square.body).toMatchObject(expected);
+  });
+  test("Method setter should set method", () => {
+    let expected = "brownies";
+    square.method = expected;
+    expect(square.method).toEqual(expected);
+  });
+  test("secret should be defined", () => {
+    expect(square.secretName).toBeDefined();
+  });
+
+  test("baseUrl should be defined", () => {
+    expect(square.baseUrl).toBeDefined();
+  });
+
+  test("url should be defined", () => {
+    expect(square.url).toBeDefined();
+  });
+});
