7fc5cb47bc23d208fdd355c34809771f59361fdf
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-01-19 19:54:24 -0800

    test: rough out unit tests - set up for expected future syntax

--- tests/catalog_request_search_objects_filter.test.js
+++ tests/catalog_request_search_objects_filter.test.js
@@ -1,9 +1,158 @@
 "use strict";
 const Catalog_Search_Filter = require("../src/lib/catalog_request_search_objects_filter");
+let filter, make;
+const class_name = "Catalog_Search_Filter";
+const method = "POST"; //http method from Square docs
+let endpoint = "/search"; //copy and paste from Square docs
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        common structures
+ *                                                         *
+ * ------------------------------------------------------- */
+describe(`${class_name} basic request class structures`, () => {
+  beforeEach(function () {
+    filter = new Catalog_Search_Filter();
+  });
+  test("should have display name", () => {
+    expect(filter._display_name).toBeDefined();
+  });
+  test("should have the method defined by Square set", () => {
+    expect(filter.method).toEqual(method);
+  });
+  test("display name should be same as class name", () => {
+    expect(filter.display_name).toEqual(class_name);
+  });
+  test("should have defined square version", () => {
+    expect(filter.square_version).toBeDefined();
+  });
+  test("should have defined _help", () => {
+    expect(filter.help).toBeDefined();
+  });
+  test("should have an endpoint", () => {
+    expect(filter.endpoint).toEqual(endpoint);
+  });
+  test("should have _delivery", () => {
+    filter.delivery = { someProp: { a: 1 } };
+    expect(filter.delivery).toBeDefined();
+  });
 
-describe("silence test suite", () => {
-  test("", () => {
-    expect("a").toEqual("a");
+  // not every request class has these
+  test("should have defined _body", () => {
+    expect(filter.body).toBeDefined();
+  });
+});
+
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        Error Checks
+ *                                                         *
+ * ------------------------------------------------------- */
+// describe (`${class_name} error checks`, () => {
+//   beforeEach (() => {
+//     myVar = new Catalog_Search_Filter();
+//     make = myVar.make ();
+//   });
+//   test ("", () => {
+//     expect (() => {
+//     }).toThrow ();
+//   });
+// });
+
+/* --------------------------------------------------------*
+ *                                                         *
+ *                        getters/setters
+ *                                                         *
+ * ------------------------------------------------------- */
+
+describe(`${class_name} getters/setters`, () => {
+  beforeEach(() => {
+    filter = new Catalog_Search_Filter();
+    make = filter.make();
+  });
+  test("make().include_related_objects () should set ", () => {
+    let expected = true;
+    make.include_related_objects(true);
+    expect(filter.include_related_objects).toEqual(expected);
+  });
+  test("make().begin_time () should set ", () => {
+    let expected = "2016-09-04T23:59:33.123Z";
+    make.begin_time(expected);
+    expect(filter.begin_time).toEqual(expected);
+  });
+  test("make().exact_query () should set ", () => {
+    let expected = {
+      attribute_name: "type",
+      attribute_value: "ITEM",
+    };
+    make.exact_query("type", "ITEM");
+    expect(filter.exact_query).toEqual(expected);
+  });
+  test("make().prefix_query () should set ", () => {
+    let expected = {
+      attribute_name: "type",
+      attribute_prefix: "ITEM",
+    };
+    make.prefix_query("type", "ITEM");
+    expect(filter.prefix_query).toEqual(expected);
+  });
+  test("make().range_query () should set ", () => {
+    let expected = {
+      attribute_name: "amount",
+      attribute_min_value: 5,
+      attribute_max_value: 30,
+    };
+    make.range_query("amount", 5, 30);
+    expect(filter.range_query).toEqual(expected);
+  });
+  test("make().sorted_attribute_query () should set ", () => {
+    let expected = {
+      attribute_name: "name",
+      initial_attribute_value: "blueberry",
+      sort_order: "ASC",
+    };
+    make.sorted_attribute_query("name", "blueberry");
+    expect(filter.sorted_attribute_query).toEqual(expected);
+  });
+  test("make().object_type () should set ", () => {
+    let expected = "";
+    make.object_type(expected);
+    expect(filter.object_type).toEqual(expected);
+  });
+  test("make().text_query () should set ", () => {
+    let val1 = "muffin";
+    let val2 = "cookie";
+    let val3 = "asparagus";
+    let expected = [val1, val2, val3];
+    make.text_query(val1).text_query(val2).text_query(val3);
+    expect(filter.text_query).toEqual(expected);
+  });
+  test("make().concat_text_query () should set ", () => {
+    let val1 = "muffin";
+    let val2 = "cookie";
+    let val3 = "asparagus";
+    let expected = [val1, val2, val3];
+    make.text_query(val1).concat_text_query([val2, val3]);
+    expect(filter.text_query).toEqual(expected);
+  });
+
+  test("make().text_query_add () should set ", () => {
+    let expected = "";
+    make.text_query_add(expected);
+    expect(filter.text_query_add).toEqual(expected);
+  });
+  test("make().text_query_remove () should remove a word ", () => {
+    let val1 = "muffin";
+    let val2 = "cookie";
+    let val3 = "asparagus";
+    let expected = [val1, val3];
+    make.text_query(val1).text_query(val2).text_query(val3);
+    make.text_query_remove(val2);
+    expect(filter.text_query).toEqual(expected);
+  });
+  test("make().limit () should set ", () => {
+    let expected = 5;
+    make.limit(expected);
+    expect(filter.limit).toEqual(expected);
   });
 });
 
