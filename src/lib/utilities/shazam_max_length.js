cf298a9f77acec1d879ec7761006f30082a29713
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-01-22 12:28:05 -0800

    refactor: change error to show first and last 20 chars

--- src/lib/utilities/shazam_max_length.js
+++ src/lib/utilities/shazam_max_length.js
@@ -3,11 +3,11 @@ Returns true if the string is less than or equal to the max length
 * */
 /** shazam_max_length validates string length, returning true if the string is equal to or less than
  * the maximum allowable length and otherwise throwing an error. If the string being checked exceeds 255
- * characters and it triggers an error, it will be truncated to the first 255 characters within the error message.
+ * characters and it triggers an error, it will be truncated to the first and last 20 characters within the error message.
  *
  * @param {number} max is the upper limit of allowable string length
  * @param {string} str is the string you want to validate
- * @param {string} displayName is the name of the class making the function call
+ * @param {string} display_name is the name of the class making the function call
  * @param {string} caller  is the name of the method making the function call
  * @throws Throws and error is the string is longer than allowed
  * @returns {boolean} Returns `true` of the string is less than or equal to the allowed limit
@@ -16,23 +16,23 @@ Returns true if the string is less than or equal to the max length
  *
  *  To check for minimum length use shazam_min_length
  * */
+
 const shazam_max_length = function (
   max,
   str = "",
-  displayName = "unspecified class",
+  display_name = "unspecified class",
   caller = "- unspecified class setter"
 ) {
   if (str.length > max) {
     let len = str.length;
     let replacement_string;
     if (str.length > 255) {
-      replacement_string = str.slice(0, 255) + "...";
+      replacement_string = str.slice(0, 20) + "..." + str.slice(-20);
     } else {
       replacement_string = str;
     }
-
     throw new Error(
-      `${displayName}.${caller} -length ${len} surpasses maximum character limit of ${max}.\nReceived: ${replacement_string}`
+      `${display_name}.${caller} -length ${len} surpasses maximum character limit of ${max}.\nReceived: ${replacement_string}`
     );
   }
   return true;
