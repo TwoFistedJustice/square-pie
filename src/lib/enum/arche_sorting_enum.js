7346957e6d3d19fedd9ad6211eab4838b5038a07
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-02-06 18:59:55 -0800

    refactor: make alias functions return original function.

--- src/lib/enum/arche_sorting_enum.js
+++ src/lib/enum/arche_sorting_enum.js
@@ -24,12 +24,24 @@ const arche_sorting_enum = {
 
   /** @function enum
    * @enum  enables a referencing class to set only allowable values on a property.
-   * @param {object} obj - pass in 'this' from the referencing class.
+   * @param {object} obj - The object to be modified
+   * @param {object} calling_this - pass in 'this' from the referencing class.
    * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
    * @example
    * #enum_PROPERTY_NAME() {
    *   return name_of_class_enum.property(this)
    * }
+   * */
+
+  /** @function sort_order()  enumerated function of arche_sorting_enum - method names are exactly the same as the property names listed
+   * in the Square docs. There may be additional methods and/or shortened aliases of other methods.
+   * @method ascending - sets sort_order to "ASC"
+   * @method up - alias of `ascending`
+   * @method oldest_first - alias of `ascending`
+   * @method descending - sets sort_order to "DESC"
+   * @method down - alias of `descending`
+   * @method newest_first - alias of `descending`
+   * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
    * */
 
   sort_order: function (obj, calling_this) {
@@ -39,28 +51,34 @@ const arche_sorting_enum = {
         return calling_this;
       },
       up: function () {
-        obj.sort_order = "ASC";
-        return calling_this;
+        return this.ascending();
       },
       oldest_first: function () {
-        obj.sort_order = "ASC";
-        return calling_this;
+        return this.ascending();
       },
       descending: function () {
         obj.sort_order = "DESC";
         return calling_this;
       },
       down: function () {
-        obj.sort_order = "DESC";
-        return calling_this;
+        return this.descending();
       },
       newest_first: function () {
-        obj.sort_order = "DESC";
-        return calling_this;
+        return this.descending();
       },
     };
   },
 
+  /** @function sort_field()  enumerated function of arche_sorting_enum - method names are exactly the same as the property names listed
+   * in the Square docs. There may be additional methods and/or shortened aliases of other methods.
+   * @method created_at sets sort_field to "CREATED_AT"
+   * @method created - alias of created_at
+   * @method updated_at sets sort_field to "UPDATED_AT"
+   * @method updated - alias of updated_at
+   * @method closed_at sets sort_field to "CLOSED_AT"
+   * @method closed - alias of closed_at
+   * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
+   * */
   sort_field: function (obj, calling_this) {
     return {
       created_at: function () {
@@ -68,24 +86,21 @@ const arche_sorting_enum = {
         return calling_this;
       },
       created: function () {
-        obj.sort_field = "CREATED_AT";
-        return calling_this;
+        return this.created_at();
       },
       updated_at: function () {
         obj.sort_field = "UPDATED_AT";
         return calling_this;
       },
       updated: function () {
-        obj.sort_field = "UPDATED_AT";
-        return calling_this;
+        return this.updated_at();
       },
       closed_at: function () {
         obj.sort_field = "CLOSED_AT";
         return calling_this;
       },
       closed: function () {
-        obj.sort_field = "CLOSED_AT";
-        return calling_this;
+        return this.closed_at();
       },
     };
   },
