a126409235c962009169a3cb0928e2de02a5956d
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-02-10 13:26:02 -0800

    refactor: arche_money()
    check for integer, add name and caller to error

--- src/lib/utilities/arche_money.js
+++ src/lib/utilities/arche_money.js
@@ -3,34 +3,41 @@ const { isISO4217 } = require("validator");
  * @param {number}  amt is the amount in the smallest currency designation (cents)
  * @param {string} currency expects a three character case-insensitive string forming an ISO 4217 compliant currency designation
  * If currency argument is not provided, currency will be set to "USD"
- * @throws Throws a TypeError if amt is a boolean
- * @throws Throws a TypeError if amt is not convertible to a Number
+ * @throws Throws a TypeError if amt is not convertible to an integer
  * @throws Throws an error if currency is not a valid ISO 4217 currency
  * @return {object}Returns a Square money object
  *
  * @typedef (object} Money
- * @property {number} An integer amount of money. Use smallest currency designation, usually cents.
- * @property {string} Three letter, ISO 4217 compliant currency code
- *
- *
+ * @property {number} amount - An integer amount of money. Use smallest currency designation, usually cents.
+ * @property {string} currency - Three letter, ISO 4217 compliant currency code
  * @example
  * You want to make EUR 42.00 (you accidentally hit shift on the last letter of "eur")
  * arche_money(4200, "euR") =>{amount: 4200, currency: "EUR"}
- *
- *
  * */
-const arche_money = function (amt, currency = "USD") {
+const arche_money = function (
+  amt,
+  currency = "USD",
+  display_name = "unspecified class",
+  caller = "unspecified method"
+) {
   let sanitized_currency = currency.trim().toUpperCase();
-  let amount = Number(amt);
-  if (isNaN(amount) || typeof amt === "boolean") {
-    throw new TypeError(`'amount' must be a number. received: ${typeof amt}`);
+  // check if amt in an integer
+  let parsed = parseInt(amt);
+  if (amt != parsed || isNaN(parsed)) {
+    throw new TypeError(
+      `${display_name}.${caller} expects an integer or a string that can be coerced to an integer. Received: ${amt}`
+    );
   }
+  // check if currency is valid
   if (!isISO4217(sanitized_currency)) {
     throw new Error(
       `Received ${currency} --  currency arg must be ISO 4217 compliant.`
     );
   }
-  return { amount, currency: sanitized_currency };
+  return {
+    amount: Number(amt),
+    currency: sanitized_currency,
+  };
 };
 
 module.exports = arche_money;
