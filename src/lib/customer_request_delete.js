f76c81569b900602c75d47b664b25004e8bd28d9
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-02-16 20:25:23 -0800

    feat: add ability to change /{id} portion of endpoint after it has been set

--- src/lib/customer_request_delete.js
+++ src/lib/customer_request_delete.js
@@ -1,16 +1,18 @@
 const Retrieve_Update_Delete = require("./customer_request_abstract_R_U_D_super");
 const {
+  endpoint_id_replace,
   shazam_is_integer,
   query_param_is_query_string,
   query_param_replace_value,
 } = require("./utilities");
 const man =
   "deletes one customer record. There is no option to delete multiple customer records.\n" +
-  "Add the Square id of the customer record to delete as an argument when you create the class. This is required. \n" +
+  "Add the Square id of the customer record to delete as an argument when you create the class. You can also do it later. \n" +
+  'To add the id after instantiation call make().id("some_id") or use the `replace_id` setter.\n' +
   "To enforce optimistic concurrency you may also add the version number of the Square customer record. To get this\n" +
   "you will have to fetch it from Square beforehand.\n" +
   "You may add the version as a second argument to the constructor or you can add it later using make().version(version)\n" +
-  "\nhttps://developer.squareup.com/reference/square/customers-api/delete-customer";
+  "https://developer.squareup.com/reference/square/customers-api/delete-customer";
 
 /** @class Customer_Delete representing an http request to delete one or more customer records
  *  @see Retrieve_Update_Delete
@@ -40,11 +42,18 @@ class Customer_Delete extends Retrieve_Update_Delete {
   set #endpoint(str) {
     this._endpoint = str;
   }
+
+  set replace_id(id) {
+    let replacement = endpoint_id_replace(this.endpoint, id);
+    this._endpoint = replacement;
+  }
+
   set version(int) {
     if (shazam_is_integer(int, this.display_name, "version"))
       this.#query_param_replace("version", int + "");
   }
   // PRIVATE METHODS
+
   #init_query_param_sequence(param, value) {
     let modified_endpoint = this.endpoint;
     // check if endpoint is already formatted as a query string.
@@ -64,10 +73,12 @@ class Customer_Delete extends Retrieve_Update_Delete {
     }
   }
 
-  /** @function make()  method of SOME_CLASS - method names are exactly the same as the property names listed
+  /** @function make()  method of Customer_Delete - method names are exactly the same as the property names listed
    * in the Square docs. There may be additional methods and/or shortened aliases of other methods.
    * @method version
    * @param {number} version - Expects an integer. Sets the 'version' query parameter
+   * @method id - replaces or set the /{id} portion of the endpoint - can be used any time.
+   * @param {string} id - the id you want to put into the endpoint.
    * @author Russ Bain <russ.a.bain@gmail.com> https://github.com/TwoFistedJustice/
    * @example
    *  You must use parentheses with every call to make and with every sub-method. If you have to make a lot
@@ -81,8 +92,13 @@ class Customer_Delete extends Retrieve_Update_Delete {
   make() {
     return {
       self: this,
+      id: function (id) {
+        this.self.replace_id = id;
+        return this;
+      },
       version: function (version) {
         this.self.version = version;
+        return this;
       },
     };
   }
