ffadbf7c9da40d4ca43bb845aa29bdbef677c999
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-02-17 19:46:14 -0800

    feat: implement function to grab all top level ids from .delivery

--- tests/square_request_abstract.test.js
+++ tests/square_request_abstract.test.js
@@ -61,4 +61,97 @@ describe("Square_Request", () => {
   test("url should be defined", () => {
     expect(square.url).toBeDefined();
   });
+
+  test("cache_ids should cache ids...", () => {
+    let response_data = [
+      {
+        id: "LJJFXHRVMV16B",
+        name: "Default Test Account",
+        address: {
+          address_line_1: "1600 Pennsylvania Ave NW",
+          locality: "Washington",
+          administrative_district_level_1: "DC",
+          postal_code: "20500",
+          country: "US",
+        },
+        timezone: "UTC",
+        capabilities: ["CREDIT_CARD_PROCESSING", "AUTOMATIC_TRANSFERS"],
+        status: "ACTIVE",
+        created_at: "2021-05-09T20:27:48.454Z",
+        merchant_id: "ML1Q8YWR7FBD0",
+        country: "US",
+        language_code: "en-US",
+        currency: "USD",
+        business_name: "Default Test Account",
+        type: "PHYSICAL",
+        business_hours: {},
+        mcc: "7299",
+      },
+      {
+        id: "LM1T0KY3GM24D",
+        name: "coffee land",
+        address: {
+          address_line_1: "401 Richmond St W",
+          locality: "Toronto",
+          administrative_district_level_1: "ON",
+          postal_code: "M5V3A8",
+          country: "US",
+        },
+        timezone: "UTC",
+        capabilities: ["CREDIT_CARD_PROCESSING", "AUTOMATIC_TRANSFERS"],
+        status: "INACTIVE",
+        created_at: "2022-02-16T20:09:42.808Z",
+        merchant_id: "ML1Q8YWR7FBD0",
+        country: "US",
+        language_code: "en-US",
+        currency: "USD",
+        phone_number: "+1 416-597-8822",
+        business_name: "Default Test Account",
+        type: "PHYSICAL",
+        business_hours: {},
+        mcc: "7299",
+      },
+      {
+        id: "L2X92PW79TPBX",
+        name: "soccer land",
+        address: {
+          address_line_1: "865 Valencia St",
+          locality: "San Francisco",
+          administrative_district_level_1: "CA",
+          postal_code: "94110",
+          country: "US",
+        },
+        timezone: "UTC",
+        capabilities: ["CREDIT_CARD_PROCESSING", "AUTOMATIC_TRANSFERS"],
+        status: "ACTIVE",
+        created_at: "2022-02-16T20:49:39.468Z",
+        merchant_id: "ML1Q8YWR7FBD0",
+        country: "US",
+        language_code: "es-US",
+        currency: "USD",
+        phone_number: "+1 555-867-5309",
+        business_name: "Default Test Account",
+        type: "MOBILE",
+        business_hours: {},
+        mcc: "7299",
+      },
+    ];
+    let expected = ["LJJFXHRVMV16B", "LM1T0KY3GM24D", "L2X92PW79TPBX"];
+
+    square.delivery = response_data;
+    square.cache_ids();
+
+    expect(square.id_array).toMatchObject(expected);
+  });
+
+  test("cache_ids should cache ids from an array of ids...", () => {
+    let response_data = ["LJJFXHRVMV16B", "LM1T0KY3GM24D", "L2X92PW79TPBX"];
+
+    let expected = ["LJJFXHRVMV16B", "LM1T0KY3GM24D", "L2X92PW79TPBX"];
+
+    square.delivery = response_data;
+    square.cache_ids();
+
+    expect(square.id_array).toMatchObject(expected);
+  });
 });
