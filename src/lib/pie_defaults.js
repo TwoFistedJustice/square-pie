cd387163f708d043709e9b8a7af4f9eaebffacd3
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-03-15 15:58:51 -0700

    refactor: make auto_set_appointment_service an imported variable

--- src/lib/pie_defaults.js
+++ src/lib/pie_defaults.js
@@ -5,6 +5,7 @@
 
 const pie_defaults = {
   uid_length: 10, // feed to nanoid for uid property complexity  smaller is faster but increases chance of collision
+  auto_set_appointment_service: false, // within Catalog_Item set auto set for product_type. True sets "APPOINTMENTS_SERVICE". false sets "REGULAR".
 };
 
 module.exports = pie_defaults;
