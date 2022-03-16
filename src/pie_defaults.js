5b34f195c1d367dadfe5f87508a3534394764892
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-03-15 19:20:23 -0700

    docs: jsdocs for pie_defaults

--- src/pie_defaults.js
+++ src/pie_defaults.js
@@ -1,11 +1,15 @@
-/*
+/**
  *  These are values used throughout Square Pie.
  *  Keep them here for easy reference and maintenance.
+ * @typedef {function} pie_defaults
+ * @public
+ * @property {number} uid_length - feed to nanoid for uid property complexity  smaller is faster but increases chance of collision
+ * @property {boolean} auto_set_appointment_service - within Catalog_Item set auto set for product_type. True sets "APPOINTMENTS_SERVICE". false sets "REGULAR".
  * */
 
 const pie_defaults = {
-  uid_length: 10, // feed to nanoid for uid property complexity  smaller is faster but increases chance of collision
-  auto_set_appointment_service: false, // within Catalog_Item set auto set for product_type. True sets "APPOINTMENTS_SERVICE". false sets "REGULAR".
+  uid_length: 10,
+  auto_set_appointment_service: false,
 };
 
 module.exports = pie_defaults;
